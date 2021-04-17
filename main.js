64191b5723ec70ed3d957c60b541b70c48bfa2f5
Aparajita-png <77184488+Aparajita-png@users.noreply.github.com>
2021-04-17 11:17:19 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -10,12 +10,12 @@ Webcam.attach('#camera');
 function take_snapshot() {
 Webcam.snap(function(data_uri){
     document.getElementById("result").innerHTML='<img id="captured_image" src="'+data_uri+'"/>';
-})
+});
 }
 console.log('ml5 version:',ml5.version);
-classifier=ml5.imageClassifier("Mobilenet",modelLoaded);
-function modelLoaded() {
-    console.log ('modelLoaded');
+classifier=ml5.imageClassifier("MobileNet",modelLoaded);
+function modelLoaded(){
+    console.log('modelLoaded');
 }
 function check() {
     img=document.getElementById("captured_image");
